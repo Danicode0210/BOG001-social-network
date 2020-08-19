46cac756a6a1dd66d4873daec3f842bf2ce4d816
yury10 <geniocrea@gmail.com>
2020-08-19 13:37:25 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
 // Este es el punto de entrada de tu aplicacion
 
-import initialSession from './views/initial-session.js';
+import initialSession from "./views/initial-session.js";
 /*  import formRegister from "./views/form-register.js"; */
-import postAdoption from './views/post-adoption.js';
+import postAdoption from "./views/post-adoption.js";
 /* import adopt from "./views/adopt.js";
 import error404 from "./views/error404.js";   */
 
 const oneView = () => {
-  document.getElementById('root').innerHTML = initialSession();
+  document.getElementById("root").innerHTML = initialSession();
 };
 oneView();
 
-const twoView = () => {
-  document.getElementById('root').innerHTML = postAdoption();
-};
-twoView();
-
+/*const twoView = () => {
+  document.getElementById("root").innerHTML = postAdoption(); 
+}; 
+twoView(); */
 
 function locationHashChanged() {
-  const location = document.getElementById('login');
-  if (location.hash === '#postadoption') {
+  const location = document.getElementById("login");
+  if (location.hash === "#postadoption") {
     postAdoption();
   }
 }
-
-document.getElementById('login').addEventListener('hashchange', locationHashChanged);
